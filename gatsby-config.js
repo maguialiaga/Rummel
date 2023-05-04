1950b0c565e538eb0eb805c1abb1b046d3643c7e
maguialiaga <maguialiaga1@gmail.com>
2023-05-04 01:38:39 -0300

    no react-helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
-    `gatsby-plugin-react-helmet`,
+
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
