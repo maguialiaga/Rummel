0bf74c7d7a497562f47fa053f984564353fde118
maguialiaga <maguialiaga1@gmail.com>
2023-05-17 12:15:00 -0300

    rummel icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,20 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
 
     {
       resolve: `gatsby-source-filesystem`,
