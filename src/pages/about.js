01b03113bd03fafd32f5cda3767c3e85f2508874
maguialiaga <maguialiaga1@gmail.com>
2023-10-06 02:37:25 +0200

    sin el start now

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Layout from "../components/Layout";
 import AboutUs from "../components/AboutUs";
-import Btn from "../components/Btn";
+// import Btn from "../components/Btn";
 
 export default function About() {
   return (
     <Layout>
       <>
         <AboutUs />
-        <Btn />
+        {/* <Btn /> */}
       </>
     </Layout>
   );
