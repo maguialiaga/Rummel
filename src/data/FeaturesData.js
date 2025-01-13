709d3e0d6e1f0e243a5487d99aa9ad434372e5c4
maguialiaga <maguialiaga1@gmail.com>
2025-01-14 00:20:38 +0100

    cambio preu

--- src/data/FeaturesData.js
+++ src/data/FeaturesData.js
@@ -9,8 +9,8 @@ export const featuresData = [
   {
     name: "Mixdown",
     description: [
-      "Up to 15 stems --- 115 EUR",
-      "15 to 30 stems --- 140 EUR",
+      "Up to 15 stems --- 120 EUR",
+      "15 to 30 stems --- 145 EUR",
       "31 to 45 stems --- 180 EUR",
       "+ than 45 stems --- 215 EUR",
     ],
@@ -19,8 +19,8 @@ export const featuresData = [
   {
     name: "Mixdown & Mastering",
     description: [
-      "Up to 15 stems --- 130 EUR",
-      "16 to 30 stems --- 155 EUR",
+      "Up to 15 stems --- 135 EUR",
+      "16 to 30 stems --- 160 EUR",
       "31 to 45 stems --- 200 EUR",
       "+ than 45 stems --- 230 EUR",
     ],
